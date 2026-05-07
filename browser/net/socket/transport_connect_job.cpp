aa461f1c784c0d75de104664d3ff22d58696f064
libo <leeboo-207@163.com>
2026-05-07 23:12:38 +0800

    transport connect job

--- browser/net/socket/transport_connect_job.cpp
+++ browser/net/socket/transport_connect_job.cpp
@@ -1,4 +1,5 @@
 #include "net/socket/transport_connect_job.h"
+#include <assert.h>
 
 namespace net {
 
@@ -7,7 +8,42 @@ TransportConnectJob::TransportConnectJob() {
 }
 
 int TransportConnectJob::ConnectInternal() {
-  return -1;
+  DoResolveHost();
+  DoTransportConnect();
+  return 0;
+}
+
+void TransportConnectJob::DoResolveHost() {
+  // 目前先跳过，用IP进行测试
+}
+
+void TransportConnectJob::DoTransportConnect() {
+  if (m_endpoint_results.empty()) {
+    assert(false);
+  }
+
+  std::vector<IPEndPoint> ipv4_addresses, ipv6_addresses;
+  ipv4_addresses.push_back(m_endpoint_results[0].ip_endpoints[0]);
+
+  ipv4_job_ = std::make_unique<TransportConnectSubJob>(
+        std::move(ipv4_addresses), this/*, SUB_JOB_IPV4*/);
+  ipv4_job_->Start();
+}
+
+
+TransportConnectSubJob::TransportConnectSubJob(std::vector<IPEndPoint> addresses,
+                         TransportConnectJob* parent_job/*,
+                         SubJobType type*/) {
+
+}
+int TransportConnectSubJob::Start() {
+  //  transport_socket_ =
+  //     parent_job_->client_socket_factory()->CreateTransportClientSocket(
+  //         one_address);
+
+  // return transport_socket_->Connect(base::BindOnce(
+  //     &TransportConnectSubJob::OnIOComplete, base::Unretained(this)));
+  return 0;
 }
 
 }
--- browser/net/socket/transport_connect_job.h
+++ browser/net/socket/transport_connect_job.h
@@ -3,8 +3,42 @@
 
 #include "net/socket/connect_job.h"
 #include <memory>
+#include <vector>
+#include <array>
 
 namespace net {
+
+class IPAddressBytes {
+public:
+  using IPAddressStorage = std::array<uint8_t, 16>;
+  IPAddressStorage bytes_;
+  uint8_t size_;
+};
+
+class IPAddress {
+public:
+  IPAddressBytes ip_address_;
+};
+
+class IPEndPoint {
+public:
+  IPAddress address_;
+  uint16_t port_ = 0;
+};
+
+struct HostResolverEndpointResult {
+  // IP endpoints at which to connect to the service.
+  std::vector<net::IPEndPoint> ip_endpoints;
+};
+
+class TransportConnectJob;
+class TransportConnectSubJob {
+public:
+  TransportConnectSubJob(std::vector<IPEndPoint> addresses,
+                         TransportConnectJob* parent_job/*,
+                         SubJobType type*/);
+  int Start();
+};
 
 class TransportConnectJob : public ConnectJob {
 public:
@@ -17,8 +51,17 @@ public:
 public:
   TransportConnectJob();
 
+  enum SubJobType { SUB_JOB_IPV4, SUB_JOB_IPV6 };
 protected:
   int ConnectInternal() override;
+  void DoResolveHost();
+  void DoTransportConnect();
+
+private:
+  std::vector<HostResolverEndpointResult> m_endpoint_results;
+
+  std::unique_ptr<TransportConnectSubJob> ipv4_job_;
+  // std::unique_ptr<TransportConnectSubJob> ipv6_job_;
 };
 
 }
